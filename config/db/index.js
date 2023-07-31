94022a31b7fb6524d4ebc2daa46d2ba6aeffa90f
xd3011 <xuandat3011@gmail.com>
2023-07-31 23:40:19 +0700

    Update and test deploy server by vercel

--- config/db/index.js
+++ config/db/index.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
+const user_name = process.env.user_name_db;
+const password = process.env.password_db;
+const database_name = process.env.database_name;
 async function connect() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1/api_mqtt");
+        await mongoose.connect(
+            `mongodb+srv://${user_name}:${password}@cluster0.7mwsy5v.mongodb.net/${database_name}?retryWrites=true&w=majority`
+        );
         console.log("Connected");
     } catch (err) {
         console.log("Error connecting");
